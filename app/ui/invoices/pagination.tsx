7f2c9c50f37a99e7e2de5130fe2e492914fd559d
YJGS-dev <yordi-futbol@hotmail.com>
2025-05-04 22:05:07 -0600

    invoices CRUD

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
-export default function Pagination({ totalPages }: { totalPages: number }) {
+export default function Pageination({ totalPages }: { totalPages: number }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
@@ -18,8 +18,6 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   };
   return (
     <>
-      {/*  NOTE: Uncomment this code in Chapter 11 */}
-
       <div className="inline-flex">
         <PaginationArrow
           direction="left"
